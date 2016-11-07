b9996c16d1631118a01a0c417f0b8ceb52e255db
dillonguarino <dillon.guarino@gmail.com>
2016-11-07 13:15:28 -0500

    Fixed merge error

--- examples/test.js
+++ examples/test.js
@@ -110,7 +110,7 @@ function Img(file, id) {
 	
 }
 
-Img.prototype.draw = function() {
+Img.prototype.draw = function(div) {
 	div.appendChild(this.img);
 }
 
@@ -215,137 +215,7 @@ function Quiz(parent, json) {
 	for (var k in json) {
 		this[k] = json[k];
 	}
-=======
-Util = {
-  subClass: function (sup, sub) {
-    sub.prototype = Object.create(sup.prototype);
-    sub.prototype.constructor = sub;
-  }
-};
-
-
-// Display element can draw itself into a div box,
-// has optional class
-function Display(id, clas) {
-	this.div;
-	this.id = id;
-	this.class = clas; // could be undefined?
-}
-
-function app(div, text) {
-	div.appendChild(document.createTextNode(text));
-}
-
-// make a div under the parent. This is a utility function for display
-// objects but not every display object necessarily creates a div
-Display.prototype.md = function(parent) {
-	this.div = document.createElement('div');
-	if (this.class)
-		this.div.class = this.class;
-	parent.appendChild(this.div);
-}
-
-
-// this method should be overridden by all children
-Display.prototype.disp = function(div) {
-	app(div, this.constructor.name + ':');
-}
-
-
-function Prefs() {
-	
-}
-
-Prefs.prototype.getPolicy = function(json) {
-	var p = this.policies[json.policy ? json.policy : "default"];
-}
-
-function Response() {
-	
-
-}
-
-Util.subClass(Display, Response);
-
-function Prefs() {}
-
-Prefs.prototype.getPolicy = function(id) {
-
-}
-
-Prefs.prototype.getRegex = function(id) {
-
-}
-
-var prefs = new Prefs();
-
-function Answer(id) {
-	
-}
-
-Util.subClass(Display, Answer);
-
-function StringAnswer(id, s) {
-	this.id = id; // do in parent
-	this.s = s;
-}
-
-StringAnswer.prototype.draw = function(div) {
-	app(div, this.s);
-}
-
-function Instr(id, s) {
-	this.id = id;
-	this.s = s;
-}
-
-Instr.prototype.draw = function(div) {
-	app(div, this.s);
-}
-
-function Eqn(id, s) {
-	this.id = id;
-	this.s = s;
-}
-
-Eqn.prototype.draw = function(div) {
-	app(div, this.s);
-}
-
-//pure audio player, no controls
-function Aud(id, file) {
-	this.audio = new Audio(file);
-}
-
-Aud.prototype.draw = function(div) { //do we want to display the audio?
-	this.audio.play();
-}
-
-function Img(id, file) {
-    this.img = document.createElement("img");
-    this.img.src = file;
-}
-
-Img.prototype.draw = function(div) {
-    div.appendChild(this.img);
-}
-
-function Fillin(id, parent) {
-
-}
-
-Fillin.prototype.draw = function() { 
-
-}
-
-function QC(parent, json) {
-	this.id = json.id;
-	this.title = json.title;
-	//TODO: inherit default from quiz, then from user (not 1)
-	this.points = (typeof json.points === 'undefined') ? 1 : json.points;
-	this.level = (typeof json.level === 'undefined') ? 1 : json.level;
->>>>>>> a0bf7e8c3cc56e53ff0fbf1a45d9df4c840f612d
-	this.md(parent);
+		this.md(parent);
 	//parent.appendChild(this.div);
 	this.policy = prefs.getPolicy(json);
 	for (var i = 0; i < this.questions.length; ++i) {

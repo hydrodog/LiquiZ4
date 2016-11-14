225f319f701344dddbe346e96664c99cd5640069
dillonguarino <dillon.guarino@gmail.com>
2016-11-14 13:53:41 -0500

    Moved questions from test.js to questions.js

--- examples/questions.js
+++ examples/questions.js
@@ -18,3 +18,66 @@ MC.prototype.draw = function(div) {
 		div.appendChild(x);
 	}
 }
+
+function Matrix(rows, cols, id) {
+	this.id = id;
+	this.cols = cols;
+	this.rows = rows;
+}
+
+Matrix.prototype.draw = function(div) {
+	var size = '50px';
+	for(var i = 0; i < this.rows; i++) {
+		var row = document.createElement("div");
+		for(var j = 0; j<this.cols; j++) {
+			var box = document.createElement("div");
+			box.style.height = size;
+			box.style.width = size;
+			box.style.border = "1px solid black";
+			box.style.display = 'inline-block';
+			var inp = document.createElement('input');
+			inp.type = 'text';
+			inp.style.width = size;
+			inp.style.height = size;
+			inp.style.textAlign = 'center';
+			box.appendChild(inp);
+			row.appendChild(box);
+		}
+		div.appendChild(row);
+	}
+}
+
+function Grid(length, id) {
+	this.id = id;
+	this.length = length;
+}
+
+Grid.prototype.draw = function(div) {
+	var size = '50px';
+	for(var i = 0; i < this.length; i++) {
+		var box = document.createElement("div");
+		box.style.height = size;
+		box.style.width = size;
+		box.style.border = "1px solid black";
+		box.style.display = 'inline-block';
+		var inp = document.createElement('input');
+		inp.type = 'text';
+		inp.style.width = size;
+		inp.style.height = size;
+		inp.style.textAlign = 'center';
+		box.appendChild(inp);
+		div.appendChild(box);
+	}
+}
+
+function Fillin(id) { //parent) {
+	this.id = id;
+	//pattern for regex
+}
+
+Fillin.prototype.draw = function(div) { 
+	var inp = document.createElement("input");
+	inp.type = "text";
+	inp.style.textAlign = 'center';
+	div.appendChild(inp);
+}
